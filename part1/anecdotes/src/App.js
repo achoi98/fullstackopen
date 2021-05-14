a9d8ec37e23fda8caab5839f8983004e474e8c5b
Andrew Choi <andrewchoi4998@gmail.com>
2021-05-13 22:30:30 -0600

    ready to submit

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -3,24 +3,56 @@ import React, { useState } from 'react'
 
 const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
 const RandomNumber = () => (Math.floor(Math.random()*(6)))
+const CopyAndIncrementArray = (array, ind) => {
+  const copy = [...array]
+  copy[ind] += 1
+  return copy
+}
+const DisplayVotes = (props) => {
+  const votes=props.value
+  return (
+    <div>
+      has {votes} votes
+    </div>
+  )
+}
+const DisplayMaximumVotes = (props) => {
+  const max = Math.max(...props.arr)
+  return (
+    <DisplayVotes value={max} />
+  )
+}
+
+const AnecdoteWithMostVotes = (props) => {
+  return (
+    props.anecdotes[props.arr.indexOf(Math.max.apply(null, props.arr))]
+  )
+}
 const App = () => {
   const anecdotes = [
+    'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
-    'The best way to get a project done faster is to start sooner',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Even the best planning is not so omniscient as to get it right the first time.',
-    'How does a project get to be a year late?... One day at a time.',
-    'Plan to throw one (implementation) away; you will, anyhow.'
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0))
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <DisplayVotes value={votes[selected]} />
       <div>
+        <Button handleClick={() => setVotes(CopyAndIncrementArray(votes, selected))} text='vote' />
         <Button handleClick={() => setSelected(RandomNumber())} text='next anecdote' />
-      </div> 
+        </div> 
+        <h1>Anecdote with most votes</h1>
+        <AnecdoteWithMostVotes arr={votes} anecdotes={anecdotes} />
+        <DisplayMaximumVotes arr={votes} />
     </div>
     )
 }
