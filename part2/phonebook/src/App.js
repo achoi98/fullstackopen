5dcfdf29830c871665d60303bbcf13da6a795cef
Andrew Choi <andrewchoi4998@gmail.com>
2021-05-17 17:11:51 -0600

    2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import backendService from './services/persons'
+import axios from 'axios'
 
 const SearchFilter = (props) => {
   return (
@@ -28,16 +29,30 @@ const PersonForm = (props) => {
     </div>
   )
 }
-
-const Person = (props) => {
+const DeleteButton = (props) => {
   return (
-    <div><p>{props.name}  {props.number}</p></div>
+    <button onClick={() => {
+      if (window.confirm(`Delete ${props.name}?`)) {
+      backendService.deleteP(props.id).then(response => {
+        if (response === 200) {
+          backendService.getAll().then(response => props.handle())
+        }
+      })  
+      }
+    }
+    }>
+      delete
+    </button>
   )
 }
-
+const Person = (props) => {
+  return (
+    <div><p>{props.name}  {props.number} {props.deleteButton}</p></div>
+  )
+}
 const DisplayPersons = (props) => {
   const filteredPersons = props.persons.filter(person => person.name.toLowerCase().includes(props.filterText.toLowerCase()))
-  const mappedFilteredPersons = filteredPersons.map(person => <Person name={person.name} number={person.number} key={person.name} />)
+  const mappedFilteredPersons = filteredPersons.map(person => <Person name={person.name} number={person.number} key={person.name} id={person.id} deleteButton={<DeleteButton name={person.name} id={person.id} handle={props.handle}/>}/>)
   return (
     <div>
       {mappedFilteredPersons}
@@ -56,7 +71,15 @@ const App = () => {
   
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.find(person => person.name === newName)) window.alert(`${newName} is already in the phonebook`)
+    const oldP = persons.find(match => match.name.toLowerCase() === newName.toLowerCase())
+    if (oldP !== undefined) {
+      if (window.confirm(`${newName} is already in the phonebook, update the number?`)) {
+        const newP = { ...oldP, number: newNumber }
+        //console.log(newP)
+        //console.log(newP.id)
+                backendService.changeN(newP).then(response => backendService.getAll().then(upd => setPersons(upd)))
+            }
+    }
     else {
       const personObject = {
       name: newName,
@@ -72,19 +95,25 @@ const App = () => {
 
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const handleFilterChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewFilter(event.target.value)
   }
+  const handleDeleteChange = () => {
+    //console.log('handleDeleteChange')
+    axios.get('http://localhost:3001/persons').then(response => setPersons(response.data))
+    
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -92,7 +121,7 @@ const App = () => {
       <h3>add a new person</h3>
       <PersonForm handleSubmit={addPerson} name={newName} number={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h3>Numbers</h3>
-      <DisplayPersons persons={persons} filterText={newFilter} />
+      <DisplayPersons persons={persons} filterText={newFilter} handle={handleDeleteChange}/>
     </div>
   )
 }
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,18 +1,30 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
-const getAll = () => {
+const GetAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-const create = newPerson => {
+const Create = newPerson => {
     const request = axios.post(baseUrl, newPerson)
     return request.then(response => response.data)
 }
 
+const DeleteP = id => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.status)
+}
 
+const ChangeNumber = newNum => {
+    //console.log(newNum)
+    const url = `http://localhost:3001/persons/${newNum.id}`
+    const request = axios.put(url, newNum)
+    return request.then(response => response.data)
+}
 export default {
-    getAll: getAll,
-    create: create,
+    getAll: GetAll,
+    create: Create,
+    deleteP: DeleteP,
+    changeN: ChangeNumber
 }
