cc545ee58c9f519143077b2a62ffb274df57d5ec
Andrew Choi <andrewchoi4998@gmail.com>
2021-05-19 14:26:30 -0600

    3.6

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -29,5 +29,46 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateID = () => (Math.floor(Math.random() * 1000000))
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body) {
+        return response.status(400).json({
+            error: 'invalid submission'
+        })
+    }
+    if (!body.name) return response.status(400).json({error: 'invalid name'})
+    if (!body.number) return response.status(400).json({error: 'invalid number'})
+    if (persons.find(person => Number(person.number) === Number(body.number))) return response.status(400).json({error: 'this number is already in the phonebook'})
+    const person = {
+        id: generateID(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
