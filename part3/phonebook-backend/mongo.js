5fb5854858650ca309c0590fa8795a8d346b0158
Andrew Choi <andrewchoi4998@gmail.com>
2021-06-15 19:10:09 -0600

    process.argv

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -18,7 +18,7 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length = 3) {
+if (process.argv.length === 3) {
     console.log('phonebook: ')
     Person.find({}).then(result => {
         result.forEach(person => {
@@ -28,14 +28,14 @@ if (process.argv.length = 3) {
     })
 }
 
-else if (process.argv.length = 5) {
+else if (process.argv.length === 5) {
     const person = new Person({
-        name: argv[3],
-        number: argv[4]
+        name: process.argv[3],
+        number: process.argv[4]
     })
 
     person.save().then(result => {
-        console.log('added ', person.name, 'number: ', person.number, 'to phonebook')
+        console.log('added ', person.name, 'number:', person.number, 'to phonebook')
         mongoose.connection.close()
     })
 }
