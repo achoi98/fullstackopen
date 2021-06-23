e96bbe3deeab3ee89f86a2da89bf7d70cb6a8858
Andrew Choi <andrewchoi4998@gmail.com>
2021-06-23 16:45:05 -0600

    4b testing backend

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
         logger.info('connected to MongoDB')
     })
@@ -17,13 +17,13 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
         logger.error('error connecting to MongoDB:', error.message)
     })
 
-    app.use(cors())
-    app.use(express.static('build'))
-    app.use(express.json())
-    app.use(middleware.requestLogger)
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-    app.use('/api/blogs', blogsRouter)
-    app.use(middleware.unknownEndpoint)
-    app.use(middleware.errorHandler)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-    module.exports = app
+module.exports = app
