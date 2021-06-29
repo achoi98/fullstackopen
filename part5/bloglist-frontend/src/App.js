2cb14eea41e8fa6297bbc4ac85605c23cd73c0ae
Andrew Choi <andrewchoi4998@gmail.com>
2021-06-29 17:35:06 -0600

    5.4 complete

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -9,7 +10,15 @@ const App = () => {
   const [username, setUsername] = useState([])
   // app state field for password
   const [password, setPassword] = useState([])
+  // app state for user 
   const [user, setUser] = useState(null)
+  // app state for notification message
+  const [notificationMessage, setNewNotificationMessage] = useState([])
+
+  // states for creating new blog post
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
 
   // initial rendering of blogs
   useEffect(() => {
@@ -28,6 +37,30 @@ const App = () => {
     }
   }, [])
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl, 
+    }
+
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(returnedBlog))
+      setNewBlogTitle('')
+      setNewBlogAuthor('')
+      setNewBlogUrl('')
+      setNewNotificationMessage(`a new blog '${returnedBlog.title}' by ${returnedBlog.author} has been added`)
+      setTimeout(() => {
+        setNewNotificationMessage(null)
+      }, 3000)
+    }
+    catch (exception) {
+      console.log('exception:', exception)
+    }
+  }
+  
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -43,7 +76,10 @@ const App = () => {
     }
     catch (exception) {
       console.log('exception:', exception)
-      alert("Wrong credentials")
+      setNewNotificationMessage('wrong credentials')
+      setTimeout(() => {
+        setNewNotificationMessage(null)
+      }, 3000)
     }
   }
 
@@ -82,10 +118,47 @@ const App = () => {
     <button onClick={handleLogout}>logout</button>
   )
 
+  const blogForm = () => (
+    // form for title input
+    <form onSubmit={addBlog}>
+      <div>
+        title:
+        <input
+        type="text"
+        value={newBlogTitle}
+        name="title"
+        onChange={({ target }) => setNewBlogTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+        type="text"
+        value={newBlogAuthor}
+        name="author"
+        onChange={({ target }) => setNewBlogAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+        type="text"
+        value={newBlogUrl}
+        name="url"
+        onChange={({ target }) => setNewBlogUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+    // form for author input
+
+    // form for url input
+  )
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification message={notificationMessage} />
         {loginForm()}
       </div>
     )
@@ -93,10 +166,18 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={notificationMessage} />
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-      {logoutButton()}
+      <div>
+        <h2>create new blog</h2>
+        {blogForm()}
+      </div>
+      <div>
+        {logoutButton()}
+      </div>
     </div>
   )
 }
