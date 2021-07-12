51f3cf2358b34d8580195fc5f065d11ae5285615
Andrew Choi <andrewchoi4998@gmail.com>
2021-07-11 20:33:21 -0600

    new action creators using redux thunk

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -2,15 +2,14 @@ import React, { useEffect } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisiblityFilter from './components/VisiblityFilter'
-import noteService from './services/notes'
 import { initializeNotes } from './reducers/noteReducer'
 import { useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
+  
   useEffect(() => {
-    noteService
-      .getAll().then(notes => dispatch(initializeNotes(notes)))
+    dispatch(initializeNotes())
   }, [dispatch])
   
   return (
